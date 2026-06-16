f9a5d6906fffc496920daca9d26d91ca0b332c7f
korbinjoe <1031700027@qq.com>
2026-06-16 19:04:18 +0800

    fix: initialize monaco typescript language

--- web/lib/monaco.ts
+++ web/lib/monaco.ts
@@ -6,15 +6,21 @@
  */
 import { loader } from '@monaco-editor/react'
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import 'monaco-editor/esm/vs/language/typescript/monaco.contribution'
 
-const { typescriptDefaults, javascriptDefaults } = monaco.languages.typescript as any
-typescriptDefaults.setDiagnosticsOptions({
-  noSemanticValidation: true,
-  noSyntaxValidation: true,
-})
-javascriptDefaults.setDiagnosticsOptions({
-  noSemanticValidation: true,
-  noSyntaxValidation: true,
-})
+const typeScriptLanguage = monaco.languages.typescript
+
+if (typeScriptLanguage) {
+  const { typescriptDefaults, javascriptDefaults } = typeScriptLanguage
+
+  typescriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: true,
+    noSyntaxValidation: true,
+  })
+  javascriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: true,
+    noSyntaxValidation: true,
+  })
+}
 
 loader.config({ monaco })
